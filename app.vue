517461a84548f7710b20a0d4d4a7d5698014c9d9
Paul H <dovernh@gmail.com>
2025-06-08 07:05:14 -0400

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<h1>Welcome to my Vue App</h1>
+		<p>This is a simple Vue.js application.</p>
+		<router-view />
+		<footer>
+			<p>&copy; 2023 My Vue App</p>
+		</footer>
+	</div>
+</template>
