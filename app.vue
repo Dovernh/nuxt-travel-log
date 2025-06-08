e63588acdeec70e23900270bccd1503579b67256
Paul H <dovernh@gmail.com>
2025-06-08 15:32:09 -0400

    landing page

--- app.vue
+++ app.vue
@@ -1,18 +1,5 @@
-<script setup lang="ts">
-import { Icon } from '#components';
-</script>
-
 <template>
-	<div class="text-center">
-		<h1>Welcome to my Vue App</h1>
-		<p>This is a simple Vue.js application.</p>
-
-		<button class="btn btn-secondary">
-			View Map <Icon name="tabler:map" />
-		</button>
-
-		<footer>
-			<p>&copy; 2023 My Vue App</p>
-		</footer>
-	</div>
+	<NuxtLayout>
+		<NuxtPage />
+	</NuxtLayout>
 </template>
